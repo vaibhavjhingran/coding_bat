7c06a20dcc1699b5191c89282e2345f019f4cb62
vaibhav.jhingran <vaibhavjhingran@gmail.com>
2019-10-30 21:31:20 +0530

    WIP: added to logic1 section continued

--- logic1.py
+++ logic1.py
@@ -26,3 +26,21 @@ def squirrel_play(temp, is_summer):
     return True
   else:
     return False
+
+# caught_speeding
+def caught_speeding(speed, is_birthday):
+  if is_birthday:
+    if speed <= 65:
+      return 0
+    elif speed in range(66, 87):
+    return 1
+    else:
+      return 2
+  else:
+    if speed <= 60:
+      return 0
+    elif speed in range(61,81):
+      return 1
+    else:
+      return 2
+
