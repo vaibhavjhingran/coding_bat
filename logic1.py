be50231c4a373b8da596eab726fdd75d429ace93
vaibhav.jhingran <vaibhavjhingran@gmail.com>
2019-10-30 22:02:59 +0530

    Added logic1 section for python

--- logic1.py
+++ logic1.py
@@ -44,3 +44,46 @@ def caught_speeding(speed, is_birthday):
     else:
       return 2
 
+# love6
+def love6(a, b):
+  return True if(a==6 or b==6 or a+b==6 or abs(a-b)==6) else False
+
+# in1to10
+def in1to10(n, outside_mode):
+  if not outside_mode:
+    if n in range(1,11):
+      return True
+    else:
+      return False
+  else:
+    if n<=1 and n>=10:
+      return True
+    else:
+      return False
+
+# near_ten
+def near_ten(num):
+  x = num%10
+  if x<=2 or x in range(8,10):
+    return True
+  else:
+    return False
+
+# sorta_sum
+def sorta_sum(a, b):
+  sum = a + b
+  return 20 if sum in range(10, 20) else sum
+
+# alarm_clock
+def alarm_clock(day, vacation):
+  if vacation:
+    if day in range(1,6):
+      return '10:00'
+    elif day==0 or day==6:
+      return 'off'
+  else:
+    if day in range(1,6):
+      return '7:00'
+    else:
+      return '10:00'
+
